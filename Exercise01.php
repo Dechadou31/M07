5439e938d00eef7e7b0abbf2ec95e1957dd7794f
Dechadou31 <marc1331cram@gmail.com>
2024-09-27 17:57:30 +0200

    first commit

---
+++ Exercise01.php
@@ -0,0 +1,33 @@
+<?php
+//Ejercicio1
+echo "1. Declare dos variables numéricas, las sume, reste, dividí y muestre los valores de las
+variables y los resultados de sus operaciones. <br>"; 
+$num1 = 16;
+$num2 = 2;
+
+$suma = $num1 + $num2;
+echo"El valor de la suma es $suma <br>";
+
+$resta = $num1 - $num2;
+echo"El valor de la resta es $resta <br>";
+
+$division = $num1 / $num2;
+echo"El valor de la division es $division <br>";
+
+echo"2. Muestra cuál es mayor, cuál menor o si son iguales. <br>";
+if ($num1 > $num2){
+    echo "El numero mayor es $num1";
+    echo "El numero menor es $num2";    
+}elseif($num1 < $num1){
+    $menor = ($num1 < $num2);
+    echo "El numero menor es $num1";
+    echo "El numero mayor es $num2";
+}else{
+    echo"Los numeros son iguales <br>";
+}
+
+echo "Si las dos variables son valores superiores a 1,
+calcula el área del triángulo con base y altura igual a los valores de las
+variables. <br>";
+
+
